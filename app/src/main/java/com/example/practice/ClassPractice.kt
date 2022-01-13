1ef8a4a0f3297825b44e27c023804758f156dbc6
skskdmsdl <skskdmsdl@gmail.com>
2022-01-13 21:23:59 +0900

    프로퍼티와 생성자, 상속

--- app/src/main/java/com/example/practice/ClassPractice.kt
+++ app/src/main/java/com/example/practice/ClassPractice.kt
@@ -4,15 +4,50 @@
 
 
 // constructor 생략 가능
-class Human constructor( val name : String = "Anonymous"){
+// 프로퍼티와 생성자 함께 사용 가능
+open class Human constructor( val name : String = "Anonymous"){
+
+    // java 오버로딩
+    /*
+     class Person {
+        public Person(String name) {
+        }
+        public Person(String name, int age){
+            this(name);
+        }
+     }
+     */
+
+    // 부 생성자는 항상 주 생성자의 위임을 받아야 함함
+   constructor(name :String, age : Int) : this(name){
+       println("my name is ${name}, ${age}years old")
+   }
+
+    init {
+        println("New human has been born!!")
+    }
+
     val name2 = "jerry" // 이름을 객체를 생성할 때 정의하고 싶다면 생성자를 이용
     //val name = name // 위에 val 한번에 선언
 
     fun eatingCake(){
         println("This is so YUMMMYYY")
     }
+
+    open fun singASong(){
+        println("lalala")
+    }
 }
 
+// 상속(extend) : 코틀린은 상속받으려는 클래스를 open해줘야 사용 가능
+// 자바와 똑같이 상속은 1개만 가능
+class Korean : Human(){
+    override fun singASong(){
+        super.singASong()
+        println("랄랄라")
+        println("my name is : ${name}")
+    }
+}
 
 fun main(){
     val human = Human("nana")
@@ -22,4 +57,10 @@ fun main(){
     println("thi human's name is ${human.name}")
     println("thi human's name is ${stranger.name}")
     println("thi human's name is ${human.name2}")
+
+    val mom = Human("Gugu", 20)
+
+    val korean = Korean()
+    korean.singASong()
+
 }
