0ce35b00a75235862c2d84da4acab83e4b49cd21
skskdmsdl <skskdmsdl@gmail.com>
2022-01-18 22:05:55 +0900

    에러 해결

--- app/src/main/java/com/example/practice/MainActivity.kt
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.practice
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,7 +10,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val button = findViewById<Button>(R.id.button)
+
         button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                TODO("Not yet implemented")
+            }
 
         })
     }
