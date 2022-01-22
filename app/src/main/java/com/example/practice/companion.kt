2b592d750c18235575f765c2af316f9aeecf6354
skskdmsdl <skskdmsdl@gmail.com>
2022-01-22 17:53:50 +0900

    companion object

--- app/src/main/java/com/example/practice/companion.kt
+++ app/src/main/java/com/example/practice/companion.kt
@@ -6,19 +6,24 @@ package com.example.practice
 
 class Book private constructor(val id : Int, val name : String){
 
-    companion object BookFactory{
+    companion object BookFactory : IdProvider{
+
+        override  fun getId() : Int {
+            return 4
+        }
         val myBook = "new book"
-        fun create() = Book(0, myBook)
+        fun create() = Book(getId(), myBook)
     }
 
 }
 
-interface  IdProvider {
-    fun getId()
+interface  IdProvider{
+    fun getId() : Int
 }
 
 fun main(){
     val book = Book.create()
 
+    val bookId = Book.BookFactory.getId()
     println("${book.id} ${book.name}")
 }
